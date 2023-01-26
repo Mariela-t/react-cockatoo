981beac72742376d585f415e22717b302af898ce
Mariela-t <tinoco.mariela01@gmail.com>
2023-01-25 21:32:49 -0800

    added new component Todo List Item, added handle to Add Todo Form, and stored New Todo in react state

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,17 +1,28 @@
 import React from "react";
+import TodoListItem from "./TodoListItem";
 
-let todoList = [
-  { id: 1, title: "Homework" },
-  { id: 2, title: "Exercise" },
-  { id: 3, title: "Study" },
-];
+// let todoList = [
+//   { id: 1, title: "Homework" },
+//   { id: 2, title: "Exercise" },
+//   { id: 3, title: "Study" },
+// ];
 
-let list = todoList.map((item) => <li key={item.id}>{item.title}</li>);
+// let list = todoList.map((item) => <li key={item.id}>{item.title}</li>);
 
 function TodoList() {
+  let todoList = [
+    { id: 1, title: "Homework" },
+    { id: 2, title: "Exercise" },
+    { id: 3, title: "Study" },
+  ];
+
   return (
     <div>
-      <ul>{list}</ul>
+      <ul>
+        {todoList.map((item) => (
+          <TodoListItem key={item.id} todoList={todoList} item={item} />
+        ))}
+      </ul>
     </div>
   );
 }
