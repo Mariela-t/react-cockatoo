286678364fa48de191447c4c76b7d59da4369ed2
Mariela-t <tinoco.mariela01@gmail.com>
2023-02-13 22:10:55 -0800

    added side-effect, custom hook and fragment

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,12 +2,6 @@ import React from "react";
 import TodoListItem from "./TodoListItem";
 
 function TodoList({ todoList }) {
-  // let todoList = [
-  //   { id: 1, title: "Homework" },
-  //   { id: 2, title: "Exercise" },
-  //   { id: 3, title: "Study" },
-  // ];
-
   return (
     <div>
       <ul>
