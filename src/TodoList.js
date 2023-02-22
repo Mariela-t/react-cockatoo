31c77e16c3a9ca53cbba155720029073542dde23
Mariela-t <tinoco.mariela01@gmail.com>
2023-02-22 15:45:44 -0800

    created reusable input with label component, Refactor Input with Label to use Component Composition, and add remove button

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,12 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
 
-function TodoList({ todoList }) {
+function TodoList({ todoList, onRemoveTodo }) {
   return (
     <div>
       <ul>
         {todoList.map((item) => (
-          <TodoListItem key={item.id} item={item} />
+          <TodoListItem key={item.id} item={item} onRemoveTodo={onRemoveTodo} />
         ))}
       </ul>
     </div>
