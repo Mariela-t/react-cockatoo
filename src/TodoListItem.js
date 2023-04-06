e5791340df0969a76d55162e16758dacd00bd4a3
Mariela-t <tinoco.mariela01@gmail.com>
2023-04-06 13:02:25 -0700

    Added simple CSS

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,12 +1,17 @@
 import React from "react";
+import styles from "./TodoListItem.module.css";
 
 function TodoListItem({ item, onRemoveTodo }) {
   return (
     <>
-      <li>
+      <li className={styles.ListItem}>
         {" "}
         {item.fields.Title}{" "}
-        <button type="button" onClick={() => onRemoveTodo(item)}>
+        <button
+          className={styles.button}
+          type="button"
+          onClick={() => onRemoveTodo(item)}
+        >
           Remove
         </button>{" "}
       </li>
